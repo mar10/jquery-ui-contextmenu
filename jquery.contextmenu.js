7f6c9e783535dab2f94e1fc35a395afa16e88160
Martin <moogle@wwwendt.de>
2013-04-02 18:55:35 +0200

    tabfix

--- jquery.contextmenu.js
+++ jquery.contextmenu.js
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * jquery.contextmenu.js plugin.
- * 
+ *
  * jQuery plugin that provides a context menu (based on the jQueryUI menu widget).
- * 
+ *
  * @see https://github.com/mar10/jquery-contextmenu
- * 
+ *
  * Copyright (c) 2013, Martin Wendt (http://wwWendt.de). Licensed MIT.
  */
 
@@ -18,7 +18,7 @@
 		version: "0.0.1",
 		options: {
 			delegate: "[data-menu]",  // selector
-			ignoreParentSelect: true, // Don't trigger 'select' for sub-menu parents 
+			ignoreParentSelect: true, // Don't trigger 'select' for sub-menu parents
 			menu: null,      // selector or jQuery or a function returning such
 			taphold: 2000, // open menu after 2000 ms long touch
 			// Events:
@@ -32,32 +32,32 @@
 			select: $.noop      // menu option was selected; return `false` to prevent closing
 		},
 		_create: function () {
-            this.element.delegate(this.options.delegate, "contextmenu.contextmenu", $.proxy(this._openMenu, this));
-            // emulate a 'taphold' event
-            /*
-            this.element.delegate(this.options.delegate, "mousedown.contextmenu", $.proxy(function(event, ui){
-                var self = this;
-                console.log("Event ", event.type, this.timer);
-                if(this.timer){
-                    console.log(" clear " + this.timer);
-                    clearTimeout(this.timer);
-                    this.timer = null;
-                }
-                this.timer = setTimeout(function(){
-                    console.log("Timeout ", event.type, self.timer);
-                    self.open.call(self, $(event.target));
-                    self.timer = null;
-                }, this.options.taphold);
-                console.log("Event started ", event.type, this.timer);
-            }, this));
-            this.element.delegate(this.options.delegate, "mouseup.contextmenu", $.proxy(function(){
-                if(this.timer){
-                    console.log("Event ", event.type, "clear" + this.timer);
-                    clearTimeout(this.timer);
-                    this.timer = null;
-                }
-            }, this));
-            */
+			this.element.delegate(this.options.delegate, "contextmenu.contextmenu", $.proxy(this._openMenu, this));
+			// emulate a 'taphold' event
+			/*
+			this.element.delegate(this.options.delegate, "mousedown.contextmenu", $.proxy(function(event, ui){
+				var self = this;
+				console.log("Event ", event.type, this.timer);
+				if(this.timer){
+					console.log(" clear " + this.timer);
+					clearTimeout(this.timer);
+					this.timer = null;
+				}
+				this.timer = setTimeout(function(){
+					console.log("Timeout ", event.type, self.timer);
+					self.open.call(self, $(event.target));
+					self.timer = null;
+				}, this.options.taphold);
+				console.log("Event started ", event.type, this.timer);
+			}, this));
+			this.element.delegate(this.options.delegate, "mouseup.contextmenu", $.proxy(function(){
+				if(this.timer){
+					console.log("Event ", event.type, "clear" + this.timer);
+					clearTimeout(this.timer);
+					this.timer = null;
+				}
+			}, this));
+			*/
 			this._trigger("init");
 		},
 		/** Return menu jQuery object. */
@@ -76,16 +76,16 @@
 		},
 		/** Open dropdown. */
 		_openMenu: function(event){
-            var self = this, 
-                $menu = this._getMenu(),
-                openEvent = event;
-		    // Prevent browser from opening the system context menu
-		    event.preventDefault();
-            // Also pass the target that the menu was triggered on as 'relatedTarget'.
-		    // This is required because our _trigger() calls will create events
-		    // that refer to the contextmenu's context (which is the target *container*) 
-            event.relatedTarget = openEvent.target;
-            
+			var self = this,
+				$menu = this._getMenu(),
+				openEvent = event;
+			// Prevent browser from opening the system context menu
+			event.preventDefault();
+			// Also pass the target that the menu was triggered on as 'relatedTarget'.
+			// This is required because our _trigger() calls will create events
+			// that refer to the contextmenu's context (which is the target *container*)
+			event.relatedTarget = openEvent.target;
+
 			if( this._trigger("beforeOpen", event) === false ){
 				return false;
 			}
@@ -99,14 +99,14 @@
 					create: $.proxy(this.options.create, this),
 					focus: $.proxy(this.options.focus, this),
 					select: function(event, ui){
-					    // Also pass the target that the menu was triggered on:
-					    event.relatedTarget = openEvent.target;
-					    var isParent = (ui.item.has(">a[aria-haspopup='true']").length > 0);
-					    if( !isParent || !self.options.ignoreParentSelect){
-	                        if( self._trigger.call(self, "select", event, ui) !== false ){
-	                            self._closeMenu.call(self);
-	                        }
-					    }
+						// Also pass the target that the menu was triggered on:
+						event.relatedTarget = openEvent.target;
+						var isParent = (ui.item.has(">a[aria-haspopup='true']").length > 0);
+						if( !isParent || !self.options.ignoreParentSelect){
+							if( self._trigger.call(self, "select", event, ui) !== false ){
+								self._closeMenu.call(self);
+							}
+						}
 					}
 				});
 			// Register global event handlers that close the dropdown-menu
@@ -121,15 +121,15 @@
 				}
 			});
 			$menu
-			    .show() // required to fix positioning error (issue #3)
+				.show() // required to fix positioning error (issue #3)
 				.css({
 					position: "absolute",
 					left: 0,
 					top: 0
 				}).position({
-					my: "left top", 
-					at: "left bottom", 
-					of: event, 
+					my: "left top",
+					at: "left bottom",
+					of: event,
 					collision: "fit"
 				}).hide()
 				.slideDown("fast", function(){
@@ -138,28 +138,28 @@
 		},
 		/** Close dropdown. */
 		_closeMenu: function(){
-			var self = this, 
+			var self = this,
 				$menu = this._getMenu();
-            if(this.timer){
-                clearTimeout(this.timer);
-                this.timer = null;
-            }
+			if(this.timer){
+				clearTimeout(this.timer);
+				this.timer = null;
+			}
 			$menu.fadeOut(function() {
 				self._trigger("close");
 			});
 		},
-        /**
-         * Handle $().contextmenu("option", ...) calls. 
-         */
-        _setOption: function(key, value){
-            $.Widget.prototype._setOption.apply(this, arguments);
-        },
-        /**
-         * Open context menu on a specific target (must match options.delegate) 
-         */
-        open: function(target){
-            var e = jQuery.Event("contextmenu", {target: target.get(0)});
-            return this.element.trigger(e);
-        }
+		/**
+		 * Handle $().contextmenu("option", ...) calls.
+		 */
+		_setOption: function(key, value){
+			$.Widget.prototype._setOption.apply(this, arguments);
+		},
+		/**
+		 * Open context menu on a specific target (must match options.delegate)
+		 */
+		open: function(target){
+			var e = jQuery.Event("contextmenu", {target: target.get(0)});
+			return this.element.trigger(e);
+		}
 	});
 } (jQuery));
