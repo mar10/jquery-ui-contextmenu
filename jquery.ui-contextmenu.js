7386158f3ae0cae1ea89789e6aac082a7f20f626
Martin@MBP <github@wwwendt.de>
2013-05-11 08:59:10 +0200

    Added taphold support

--- jquery.ui-contextmenu.js
+++ jquery.ui-contextmenu.js
@@ -14,34 +14,15 @@
 //		$menu = $(menu);
 //		return $menu.data("ui-menu") || $menu.data("menu");
 //	}
-/*
-	var startTime, endTime;
-	var gbMove = false;
+	var NS = ".contextmenu";
 
-	window.addEventListener('touchstart',function(event) {
-		startTime = new Date().getTime();
-		gbMove = false;
-		alert('tap hold s event');
-	}, false);
-
-	window.addEventListener('touchmove',function(event) {
-	  gbMove = true;
-	}, false);
-
-	window.addEventListener('touchend',function(event) {
-		endTime = new Date().getTime();
-		if(!gbMove && (endTime-startTime)/1000 > 2){
-			alert('tap hold event');
-		}
-	}, false);
-*/
 	$.widget("ui.contextmenu", {
-		version: "0.2.0",
+		version: "0.3.0",
 		options: {
 			delegate: "[data-menu]",  // selector
 			ignoreParentSelect: true, // Don't trigger 'select' for sub-menu parents
-			menu: null,      // selector or jQuery or a function returning such
-			taphold: 800, // open menu after 2000 ms long touch
+			menu: null,         // selector or jQuery or a function returning such
+			taphold: true,      // open menu on taphold events (requires external plugins)
 			// Events:
 			beforeOpen: $.noop, // menu about to open; return `false` to prevent opening
 			blur: $.noop,       // menu option lost focus
@@ -53,65 +34,18 @@
 			select: $.noop      // menu option was selected; return `false` to prevent closing
 		},
 		_create: function () {
-			var opts = this.options;
+			var opts = this.options,
+				eventNames = "contextmenu" + NS;
+			if(opts.taphold){
+				eventNames += " taphold" + NS;
+			}
 			if($.isArray(opts.menu)){
 				this.orgMenu = opts.menu;
 				opts.menu = $.ui.contextmenu.createMenuMarkup(opts.menu);
 			}
-			this.element.delegate(this.options.delegate, "contextmenu.contextmenu", $.proxy(this._openMenu, this));
+
+			this.element.delegate(this.options.delegate, eventNames, $.proxy(this._openMenu, this));
 			// emulate a 'taphold' event
-/*
-			var tapStartHandler = function(event){
-				console.log("Event ", event.type, this.tapTimer);
-				tapClearHandler(event);
-				this.tapTimer = setTimeout(function(){
-					console.log("Timeout ", event.type, this.tapTimer, event.target);
-					alert("Timeout " + event.type + this.tapTimer + " " + $(event.target).text());
-					this.open.call(this, $(event.target));
-					this.tapTimer = null;
-				}, this.options.taphold);
-				console.log("Event started ", event.type, this.tapTimer);
-			};
-			var tapClearHandler = function(event){
-				if(this.tapTimer){
-					console.log("clear " + this.tapTimer);
-					clearTimeout(this.tapTimer);
-					this.tapTimer = null;
-				}
-			};
-			var tapEndHandler = function(event){
-				if(this.tapTimer){
-					tapClearHandler(event);
-					return false;
-				}
-			};
-			this.element
-				.delegate(this.options.delegate, "touchstart.contextmenu", $.proxy(tapStartHandler, this))
-				.delegate(this.options.delegate, "touchend.contextmenu", $.proxy(tapEndHandler, this))
-				.delegate(this.options.delegate, "touchmove.contextmenu", $.proxy(tapClearHandler, this));
-*/
-//			this.element.delegate(this.options.delegate, "touchstart.contextmenu", $.proxy(function(event, ui){
-//				var self = this;
-//				console.log("Event ", event.type, this.tapTimer);
-//				if(this.tapTimer){
-//					console.log(" clear " + this.tapTimer);
-//					clearTimeout(this.tapTimer);
-//					this.tapTimer = null;
-//				}
-//				this.tapTimer = setTimeout(function(){
-//					console.log("Timeout ", event.type, self.tapTimer);
-//					self.open.call(self, $(event.target));
-//					self.tapTimer = null;
-//				}, this.options.taphold);
-//				console.log("Event started ", event.type, this.tapTimer);
-//			}, this));
-//			this.element.delegate(this.options.delegate, "touchend.contextmenu", $.proxy(function(){
-//				if(this.tapTimer){
-//					console.log("Event ", event.type, "clear" + this.tapTimer);
-//					clearTimeout(this.tapTimer);
-//					this.tapTimer = null;
-//				}
-//			}, this));
 			this._trigger("init");
 		},
 		/**
@@ -183,11 +117,11 @@
 					}
 				});
 			// Register global event handlers that close the dropdown-menu
-			$(document).bind("keydown.contextmenu", function(event){
+			$(document).bind("keydown" + NS, function(event){
 				if( event.which === $.ui.keyCode.ESCAPE ){
 					self._closeMenu();
 				}
-			}).bind("mousedown.contextmenu touchstart.contextmenu", function(event){
+			}).bind("mousedown" + NS + " touchstart" + NS, function(event){
 				// Close menu when clicked outside menu
 				if( !$(event.target).closest(".ui-menu-item").length ){
 					self._closeMenu();
@@ -225,6 +159,7 @@
 		 * Open context menu on a specific target (must match options.delegate)
 		 */
 		open: function(target){
+			// Fake a contextmenu event
 			var e = jQuery.Event("contextmenu", {target: target.get(0)});
 			return this.element.trigger(e);
 		},
